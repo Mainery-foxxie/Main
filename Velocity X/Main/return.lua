c0f99e14ff6b5ec6b16d0f4ea2d8e3d506c819a0
Mainery-foxxie <hilosussy@gmail.com>
2026-04-09 19:21:28 +0700

    Update return.lua

--- Velocity X/Main/return.lua
+++ Velocity X/Main/return.lua
@@ -1,1 +1,10 @@
-
+print(identifyexecutor())
+if string.find(identifyexecutor(), "Arceus X") or
+   string.find(identifyexecutor(), "Xeno") or
+   string.find(identifyexecutor(), "Solara") or
+   string.find(identifyexecutor(), "Seliware") or
+   string.find(identifyexecutor(), "Elternia") then
+    print("You executor doesn't support this function")
+else
+    print("hi")
+end
