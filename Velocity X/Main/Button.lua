2989ef0f9d271b5f43d7a4b6b65ef73109a46062
Mainery-foxxie <hilosussy@gmail.com>
2026-04-10 21:55:04 +0700

    Update Button.lua

--- Velocity X/Main/Button.lua
+++ Velocity X/Main/Button.lua
@@ -1,7 +1,8 @@
 task.spawn(function()
     while task.wait() do
         pcall(function()
-            local c = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+            local player = game.Players.LocalPlayer
+            local c = player.Character or player.CharacterAdded:Wait()
             local h = c:WaitForChild("HumanoidRootPart")
 
             for _, p in ipairs(workspace:GetDescendants()) do
@@ -9,7 +10,7 @@ task.spawn(function()
                     local parent = p.Parent
                     if parent and parent:IsA("BasePart") then
                         h.CFrame = parent.CFrame + Vector3.new(0, 3, 0)
-                        task.wait(0.1)
+                        task.wait(0.1) 
                         fireproximityprompt(p)
                     end
                 end
@@ -21,13 +22,15 @@ end)
 task.spawn(function()
     while task.wait() do
         pcall(function()
-            local c = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+            local player = game.Players.LocalPlayer
+            local c = player.Character or player.CharacterAdded:Wait()
             local h = c:FindFirstChildOfClass("Humanoid")
+
             if h and h.Health <= 70 then
-                game.Players.LocalPlayer:kick("hi") 
+                player:Kick("hi")
                 game:GetService("TeleportService"):Teleport(
                     game.PlaceId,
-                    game.Players.LocalPlayer
+                    player
                 )
             end
         end)
@@ -37,13 +40,13 @@ end)
 task.spawn(function()
     while task.wait() do
         pcall(function()
-                 wait(1) 
-                    game.Players.LocalPlayer:kick("hi")
-                game:GetService("TeleportService"):Teleport(
-                    game.PlaceId,
-                    game.Players.LocalPlayer
-                )
-            end
+            wait(2)
+            local player = game.Players.LocalPlayer
+            player:Kick("hi")
+            game:GetService("TeleportService"):Teleport(
+                game.PlaceId,
+                player
+            )
         end)
     end
 end)
