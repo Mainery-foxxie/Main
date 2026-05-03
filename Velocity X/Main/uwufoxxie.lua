4a6ecbb37fde3e30d2a079129b9512f2e84f3e1a
Mainery-foxxie <hilosussy@gmail.com>
2026-05-04 01:43:05 +0700

    Create uwufoxxie.lua

---
+++ Velocity X/Main/uwufoxxie.lua
@@ -0,0 +1,3 @@
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Tower.WinPart.CFrame
+wait(1) 
+game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
