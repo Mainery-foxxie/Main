8b5379f4fac9807984440016a29aa1fc83a61352
Mainery-foxxie <hilosussy@gmail.com>
2026-05-04 01:51:37 +0700

    Update uwufoxxie.lua

--- Velocity X/Main/uwufoxxie.lua
+++ Velocity X/Main/uwufoxxie.lua
@@ -1,3 +1,25 @@
-game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Tower.WinPart.CFrame
-wait(1) 
-game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local Player = Players.LocalPlayer
+
+while task.wait() do
+    pcall(function()
+        local Character = Player.Character or Player.CharacterAdded:Wait()
+        local HRP = Character:WaitForChild("HumanoidRootPart")
+        local WinPart = workspace:WaitForChild("Tower"):WaitForChild("WinPart")
+
+        HRP.CFrame = WinPart.CFrame
+    end)
+
+    task.wait(1)
+
+    local success, err = pcall(function()
+        TeleportService:Teleport(game.PlaceId, Player)
+    end)
+
+    if not success then
+        warn("Teleport failed: ".. tostring(err))
+        task.wait(3)
+    end
+end
